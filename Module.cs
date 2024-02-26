0903f32581a962ed66719ade806c768829c32c77
alext <alext@DESKTOP-8C0Q9OR>
2024-02-26 13:28:11 -0500

    Add project files.

---
+++ Module.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using Alexandria;
+using Alexandria.ItemAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Mod
+{
+    [BepInDependency(Alexandria.Alexandria.GUID)] // this mod depends on the Alexandria API: https://enter-the-gungeon.thunderstore.io/package/Alexandria/Alexandria/
+    [BepInDependency(ETGModMainBehaviour.GUID)]
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Module : BaseUnityPlugin
+    {
+        public const string GUID = "creator.etg.modname";
+        public const string NAME = "MOD NAME";
+        public const string VERSION = "0.0.0";
+        public const string TEXT_COLOR = "#00FFFF";
+
+        public void Start()
+        {
+            ETGModMainBehaviour.WaitForGameManagerStart(GMStart);
+        }
+
+        public void GMStart(GameManager g)
+        {
+            ExamplePassive.Register();
+            Log($"{NAME} v{VERSION} started successfully.", TEXT_COLOR);
+        }
+
+        public static void Log(string text, string color="#FFFFFF")
+        {
+            ETGModConsole.Log($"<color={color}>{text}</color>");
+        }
+    }
+}
